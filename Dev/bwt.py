45d2e2226e7db6e4c9a4bbf9de1ceadc6a35f93d
Vasilije Becic <vbecicpro@yahoo.com>
2022-05-22 20:06:50 +0200

    Add BWT and FM Query

---
+++ Dev/bwt.py
@@ -0,0 +1,110 @@
+'''Class used for performing Burrows-Wheeler transformation.'''
+class BWT:
+    transformedText = '' 
+    saIndexes = []
+    tots = dict()
+    ranks = []
+    firstCol = dict()
+    
+    
+    def clear_parameters(self):
+        self.transformedText = '' 
+        self.saIndexes = []
+        self.tots = dict()
+        self.ranks = []
+        self.firstCol = dict()
+        
+
+    def transform(self, text):
+        '''Performs BW transformation and keeps the result in transformedText and saIndexes.
+        Also, initializes tots (pairs char:#), ranks (for transformedText).'''
+        self.clear_parameters()
+        self.transformedText = self.bwt_via_sa(text) # Initializes transformedText, saIndexes
+        self.rank_bwt()  # Initializes tots, ranks
+        self.calculate_first_col() # Initializes firstCol
+        
+        
+    def suffix_array(self, t):
+        """ Given T return suffix array SA(T).  We use Python's sorted
+            function here for simplicity, but we can do better. """
+        saMatrix = sorted([(t[i:], i) for i in range(len(t))])
+        # Extract and return just the offsets
+        #     print(satups)
+        return list(map(lambda x: x[1], saMatrix))
+    
+    
+    def bwt_via_sa(self, t):
+        """ Given T, returns BWT(T) by way of the suffix array. """
+        bw = []
+        self.saIndexes = self.suffix_array(t)
+        for si in self.saIndexes:
+            if si == 0: bw.append('$')
+            else: bw.append(t[si-1])
+        return ''.join(bw) # return string-ized version of list bw
+    
+    
+    def rank_bwt(self):
+        ''' Given BWT string bw, return parallel list of B-ranks.  Also
+        returns tots: map from character to # times it appears. 
+        Initializes tots and ranks.'''
+        self.tots = dict()
+        self.ranks = []
+        for c in self.transformedText:
+            if c not in self.tots: self.tots[c] = 0
+            self.ranks.append(self.tots[c])
+            self.tots[c] += 1
+    
+    
+    def calculate_first_col(self):
+        ''' Return map from character to the range of rows prefixed by
+        the character. Initializes first column.'''
+        self.firstCol = {}
+        totc = 0
+        for c, count in sorted(self.tots.items()):
+            self.firstCol[c] = (totc, totc + count)
+            totc += count
+    
+    
+    def last_col_with_ranks(self):
+        ''' Returns list with tuples (rank, char) for lastColumn.'''
+        return list(zip(self.ranks, self.transformedText))
+    
+    
+    def original_text(self):
+        ''' Make T from BWT(T) '''
+        rowi = 0 # Start in first row
+        t = '$' # Start in rightmost character
+        while self.transformedText[rowi] != '$':
+            c = self.transformedText[rowi]
+            t = c + t # Prepend to answer
+            # Jump to row that starts with c of same ranke
+            rowi = self.firstCol[c][0] + self.ranks[rowi]
+        return t
+
+
+'''
+# TESTS
+text = 'abaaba$'
+bwt = BWT()
+bwt.transform(text)
+original = bwt.original_text()
+
+print("Original: " + original)
+print()
+# Expected: 'abaaba$'
+
+print('First col')
+print(bwt.firstCol)
+print()
+# Expected: {'$': (0, 1), 'a': (1, 5), 'b': (5, 7)}
+
+print('SaIndexes')
+print(bwt.saIndexes)
+print()
+# Expected: [6, 5, 2, 3, 0, 4, 1]
+
+print('Last col with ranks')
+print(bwt.last_col_with_ranks())
+print()
+# Expected: [(0, 'a'), (0, 'b'), (1, 'b'), (1, 'a'), (0, '$'), (2, 'a'), (3, 'a')]
+'''
---
+++ Dev/fm.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 17:39:34 2022
+
+@author: vbeci
+"""
+
+from bwt import BWT
+
+class FMIndex:
+    
+    bwt = {}
+    tally = dict() # Key = character, Value = occurences of character (len of BWT(T))
+    c = dict()  # Key = character, Value = (value for first row occurence)
+    
+    def __init__(self, text):
+        text = text + '$'
+        self.clear_parameters()
+        self.bwt = BWT() # We only care about L and SA
+        self.bwt.transform(text)
+        self.init_tally() # Initialize tally
+        self.init_c() # Initialize c
+        
+    
+    def clear_parameters(self):
+        self.bwt = {}
+        self.tally = dict()
+        self.c = dict()
+        
+    
+    def init_tally(self):
+        empty = []
+        occurences = dict()
+        for i in range(len(self.bwt.transformedText)):
+            empty.append(0)
+            occurences[self.bwt.transformedText[i]] = 0
+            i += 1
+            
+        for char in self.bwt.firstCol.keys():
+            self.tally[char] = empty.copy()
+            
+        for i in range(len(self.bwt.transformedText)):
+            character = self.bwt.transformedText[i]
+            occurences[character] += 1
+            
+            for char in self.tally.keys():
+                self.tally[char][i] = occurences[char]
+                
+
+    def init_c(self):
+        ''' Initializes c dictionary, where key = character 
+        and value = tuple (index, value for first row occurence)'''
+        i = 0
+        for char in self.bwt.firstCol.keys():
+            self.c[char] = (i, self.bwt.firstCol[char][0])
+            i += 1
+            
+    
+    def query(self, pattern):
+        ''' Returns list of found positions for pattern in the text. 
+        Rows are counted from 0 to n, not from 1.'''
+        positions = []
+        start = 1
+        end = len(self.bwt.transformedText) - 1
+        k = len(pattern) - 1
+        while k > -1:
+            ch = pattern[k]
+            # start = C[ch], end = C[ch + 1] with break if ch + 1 > all keys
+            if k == len(pattern) -1:
+                start = self.c[ch][1]
+                if self.c[ch][0] == len(self.c) -1:
+                    break
+                # Find C[ch + 1]
+                index = self.c[ch][0] + 1
+                newKey = ''
+                for key in self.c:
+                    if self.c[key][0] == index:
+                        newKey = key
+                        break
+                end = self.c[newKey][1] - 1
+            else:
+                start = self.c[ch][1] + self.tally[ch][start - 1]
+                end = self.c[ch][1] + self.tally[ch][end] - 1
+                
+            k -= 1
+        for i in range(start, end + 1):
+            positions.append(self.bwt.saIndexes[i])
+        return sorted(positions)
+        
+    
+
+# TESTS
+text = 'abaaba' # Example - Expected
+# text = 'GATGCGAGAGATG'
+# text = 'BANANA'
+fmIndex = FMIndex(text)
+
+# print('Tally:')
+# print(fmIndex.tally)
+# print()
+# Expected {'$': [0, 0, 0, 0, 1, 1, 1], 'a': [1, 1, 1, 2, 2, 3, 4], 'b': [0, 1, 2, 2, 2, 2, 2]}
+
+# print('C')
+# print(fmIndex.c)
+# print()
+# Expected {'$': (0, 0), 'a': (1, 1), 'b': (2, 5)}
+
+pattern = 'aba'
+# pattern = 'GAGA'
+# pattern = 'ANA'
+print('Query for pattern: ' + pattern)
+print(fmIndex.query(pattern))
+print()
+# Expected
+# For text='GAGA' -> [5,7]
+# For text='ANA' -> [1,3]
+
+
+
+
